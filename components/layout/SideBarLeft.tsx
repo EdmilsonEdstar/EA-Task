e6ae9b7230f7147ae8c01fd98ee0d4cd16a33c41
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2026-03-10 22:04:37 +0000

    implement Navegation fixed

--- components/layout/SideBarLeft.tsx
+++ components/layout/SideBarLeft.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function SideBarLeft() {
   return (
     <>
-        <div className="sm:static absolute bottom-0 w-[100%] lg:w-[15%] lg:h-[100vh] px-6 pt-4  bg-white border-r-2 border-gray-100">  
+        <div className="sm:fixed absolute bottom-0 w-[100%] lg:w-[15%] lg:h-[100vh] px-6 pt-4  bg-white border-r-2 border-gray-100">  
             <div className="mb-10 sm:static hidden lg:block">
                 <h1 className="text-2xl font-bold"><span className="text-amber-400">EA</span>Task</h1> 
             </div>    
-            <ul className="flex flex-row lg:flex-col justify-between lg:gap-0 space-y-4">
+            <ul className="flex flex-row lg:flex-col justify-between lg:gap-0 space-y-4 text-sm l">
                 <li>
                     <Link href="" className="flex flex-col lg:flex-row items-center gap-2 hover:text-amber-400 transition-all">
                         <Home size={18} /> Dashboard
