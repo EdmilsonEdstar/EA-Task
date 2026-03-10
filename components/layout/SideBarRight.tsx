04a4c141921ccc67e8982f0b37539e38a1091099
EdmilsonEdstar <edmilsonantoniooficial@gmail.com>
2026-03-10 21:11:24 +0000

    fix:mobile interface Responsivenes

--- components/layout/SideBarRight.tsx
+++ components/layout/SideBarRight.tsx
@@ -6,7 +6,7 @@ import DashboardTable from "../modules/dashboard/Table";
 export default function SideBarRight() {
   return (
     <>
-        <div className="h-[95vh] lg:h-[100vh] lg:w-[95%] bg-gray-50 space-y-4">      
+        <div className="h-[100vh] w-[100%] lg:w-[95%] bg-gray-50 space-y-4 overflow-y-auto custom-scrollbar">      
               <div className="bg-white py-2 flex gap-2 justify-between items-center px-6 border-b-2 border-gray-100">
                   <div>
                       <h1 className="text-2xl font-bold lg:hidden"><span className="text-amber-400">EA</span>Task</h1>
@@ -15,9 +15,8 @@ export default function SideBarRight() {
                       <button type="button" aria-label="User profile" className="flex flex-row justify-center items-center gap-2  text-black px-6 py-3 rounded-sm"><PlusSquare size={18} />Add Task</button>
                       <button type="button" aria-label="User profile" className="w-10 h-10 flex items-center justify-center bg-gray-700 hover:bg-gray-600 text-amber-300 rounded-full transition"><User size={18} /></button>         
                   </div>
-
               </div>
-              <div className="flex flex-row justify-between px-6">
+              <div className="flex flex-col lg:flex-row justify-between px-6 space-y-2">
                   <DashboardCard icon={<ListTodo size={20} />} qty="12" desc="Open Tasks" />
                   <DashboardCard icon={<Clock size={20} />} qty="8" desc="In Progress" />
                   <DashboardCard icon={<CheckSquare size={20} />} qty="25" desc="Completed" />
--- components/modules/dashboard/DashboardCard.tsx
+++ components/modules/dashboard/DashboardCard.tsx
@@ -9,11 +9,11 @@ type PropsDashboardCard = {
 export default function DashboardCard(props:PropsDashboardCard) {
   return (
     <>
-        <div className="w-[32%] flex justify-between flex-wrap bg-white text-sm lg:text-base shadow p-4 space-y-2 rounded-sm">
+        <div className="w-[100%] lg:w-[32%] flex justify-between flex-wrap bg-white text-sm lg:text-base shadow p-4 space-y-2 rounded-sm">
             <div className="space-y-2">
                 <p>{props.desc}</p>
                 <h1 className="font-bold text-gray-700 text-4xl">{props.qty}</h1>   
-            </div>
+            </div> 
             <div>
                 <p className="bg-amber-400 inline-block p-2 rounded-sm text-amber-100  border border-2 border-white">{props.icon}</p>
             </div>
